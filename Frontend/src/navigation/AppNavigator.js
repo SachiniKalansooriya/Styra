3bf17b4714b0e9749b613c64e4f3267493fc08e6
SachiniKalansooriya <sachinikalansooriya22@gmail.com>
2025-09-04 13:48:02 +0530

    backend initialization

---
+++ Frontend/src/navigation/AppNavigator.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { LandingScreen } from '../screens/LandingScreen';
+import { LoginScreen } from '../screens/LoginScreen';
+import { SignUpScreen } from '../screens/SignUpScreen';
+import HomeScreen from '../screens/HomeScreen';
+import { AddClothesScreen } from '../screens/AddClothesScreen';
+import MyWardrobeScreen from '../screens/MyWardrobeScreen';
+import TripPlannerScreen from '../screens/TripPlannerScreen';
+import GetOutfitScreen from '../screens/GetOutfitScreen';
+
+const Stack = createStackNavigator();
+
+export const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Landing"
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+          cardStyleInterpolator: ({ current, layouts }) => {
+            return {
+              cardStyle: {
+                transform: [
+                  {
+                    translateX: current.progress.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [layouts.screen.width, 0],
+                    }),
+                  },
+                ],
+              },
+            };
+          },
+        }}
+      >
+        <Stack.Screen name="Landing" component={LandingScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="AddClothes" component={AddClothesScreen} />
+        <Stack.Screen name="MyWardrobe" component={MyWardrobeScreen} />
+        <Stack.Screen name="TripPlanner" component={TripPlannerScreen} />
+        <Stack.Screen name="GetOutfit" component={GetOutfitScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
